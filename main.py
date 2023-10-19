0dfc1d7fc69dfe085bf1e8430af83e00d0cc6460
CraftyRyte <9210@sanskritischool.edu.in>
2023-10-19 10:31:58 +0530

    Added Framerate independence

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pdb
+
 import pygame as pyg
 import random
 
@@ -7,31 +9,26 @@ class Fruit:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.type = random.choices(['u', 'h'], weights=[0.3, .7])
-        self.color = ''
-        self.vel = 2
+        self.type = random.choices(['u', 'h'], weights=[0.3, 0.7])
+        self.color = (0, 0, 0) if self.type == ['u'] else (255, 255, 255)
         self.rect = pyg.Rect(x, y, 20, 20)
-        if self.type == ['u']:
-            self.color = (0, 0, 0)
-        else:
-            self.color = (255, 255, 255)
+        self.vel = 200  # Adjust this value for gravity strength
 
-    def draw(self):
-        self.rect = pyg.Rect(self.x, self.y, 20, 20)
+    def update(self, dt):
+        self.y += self.vel * dt / 1000
+        self.vel += 400 * dt / 1000  # Adjust this value for gravity strength
+        self.rect.topleft = (self.x, self.y)
+
+    def draw(self, screen):
         pyg.draw.ellipse(screen, self.color, self.rect)
 
-    def grav(self):
-        self.y += self.vel
-        self.vel += 0.5
 
-    def update(self):
-        self.grav()
-        self.draw()
-
+# Initialize Pygame
+pyg.init()
 
 # Window
-dimensions = [600, 800]
-screen = pyg.display.set_mode(tuple(dimensions))
+dimensions = [300, 400]
+screen = pyg.display.set_mode(tuple(dimensions), pyg.RESIZABLE)
 pyg.display.set_caption("Health Ninja")
 
 # Game loop and vars
@@ -39,30 +36,39 @@ clock = pyg.time.Clock()
 is_running = True
 spawn_rate = 1
 spawn_time = 0
-fruits = [Fruit(300, -20)]
+fruits = []
+
+last_time = pyg.time.get_ticks()
 
 while is_running:
-    clock.tick(30)
+    dim = pyg.display.get_window_size()
     for event in pyg.event.get():
         if event.type == pyg.QUIT:
             is_running = False
+
     screen.fill((200, 157, 124))
 
-    # spawning mech
-    if pyg.time.get_ticks() > spawn_time:
-        rand_pos = (random.randint(20, dimensions[0] - 20), -20)
+    # Calculate time passed since the last frame
+    current_time = pyg.time.get_ticks()
+    dt = current_time - last_time
+    last_time = current_time
+
+    # Spawning mechanism
+    if current_time > spawn_time:
+        rand_pos = (random.randint(20, dim[0] - 20), -20)
         fruits.append(Fruit(rand_pos[0], rand_pos[1]))
         spawn_time += spawn_rate * 1000
-        spawn_rate -= 0.01
+        if spawn_rate > 0.19:
+            spawn_rate -= 0.1
+
     for i in fruits:
-        if i.y > dimensions[1]:
+        if i.y > dim[1]:
+            # pdb.set_trace()
             fruits.remove(i)
-            del i
-            continue
-        i.update()
-        # print(fruits)
+        else:
+            i.update(dt)
+            i.draw(screen)
 
     pyg.display.update()
-
 pyg.quit()
-exit()
+
